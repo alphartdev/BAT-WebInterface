8a01a6c05da1a1fe22078a9d2b8e6c4da58f1c01
Alphart <alphartdev@gmail.com>
2014-11-09 17:12:03 +0100

    Update index.php
    Bump to 1.3 (it wasn't really 1.1 before)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * BungeeAdminTools - WebInterface
  * Author : Alphart
- * Version 1.1
+ * Version 1.3
  * License : GPL V3 -> https://github.com/alphartdev/BungeeAdminTools/blob/master/LICENSE
  */
 if(is_dir("__install")){
