32ae4f64aeee1c72f243960e09366affe768ae81
Alphart <alphartdev@gmail.com>
2014-12-25 10:24:53 +0100

    Add debug mode

--- index.php
+++ index.php
@@ -13,9 +13,47 @@ if(is_dir("__install")){
 // Load Composer autoload
 require_once("vendor/autoload.php");
 
+// Loading config.inc.php with composer doesn't seem to keep variable in the right scope
+require("application/config/config.inc.php");
+if($debugMode){
+	echo "<h3><center><span class ='glyphicon glyphicon-warning-sign'></span>
+			The debug mode is enabled ! Disable it from the configuration file.
+		<span class ='glyphicon glyphicon-warning-sign'></span></center></h3>";
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
+}
+if($increaseMemoryLimit){
+	ini_set('memory_limit', '16M');
+}
+
+// Use the given timezone if there is one
+$isTimezoneSet = true;
+if($timezone != "default"){
+	if(!date_default_timezone_set($timezone)){
+		$isTimezoneSet = false;
+	}
+}
+// If the timezone isn't set by the php.ini, we set it to UTC
+try{
+    date_default_timezone_get();
+}
+catch(Exception $e){
+	$isTimezoneSet = false;
+    date_default_timezone_set('UTC');
+}
+if(!$isTimezoneSet){
+	echo "<h3><center><span class ='glyphicon glyphicon-warning-sign'></span>
+			The specified timezone is not valid. Please change it in the configuration (timezone settings).
+		<span class ='glyphicon glyphicon-warning-sign'></span></center></h3>";
+}
+
 session_start();
 
 $router = new Router($_GET);
 $controller = $router->getController();
 
 $controller->executeAction();
+
+if($debugMode){
+	echo "<h3><center>Memory peak usage :" . floor((memory_get_peak_usage() / 1024)) . "Kb</center></h3>";
+}
