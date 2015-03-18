d65954a7d9f2ca879b778268dfe0bb3ac6270b2c
electronicboy <theboyetronic@gmail.com>
2015-03-18 16:06:41 -0700

    Fixed up some English I missed

--- application/views/_template/errorPage.php
+++ application/views/_template/errorPage.php
@@ -1,4 +1,4 @@
 <div class="container jumbotron">
-<h2 style="text-align: center; color: navy;">An error occured</h1>
+<h2 style="text-align: center; color: navy;">An error has occured</h1>
 <?php if(!empty($errorDetails)) echo "<p>" . $errorDetails . "</p>";?>
 </div>
