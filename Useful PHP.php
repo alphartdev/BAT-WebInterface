c659cf913dd898615b4da87eeedbc90bc048287b
Muhsinun Chowdhury <muhsinun08@gmail.com>
2017-01-03 16:31:15 -0500

    add custom navbar

--- Useful PHP.php
+++ Useful PHP.php
@@ -5,3 +5,12 @@ Get username: <?php echo $this->getUsername();?>
 Check if player is admin: <?php if($this->isSU()){?> INPUT CONTENT FOR ADMIN EYES ONLY HERE Then end admin content with: <?php }?>
 OR instead of ending ALL content, you can use this: <?php }else{?> instead of this <?php }?> , input content for non-admins, and THEN put <?php }?> to end ALL content.
 This is helpful to stop an error saying that, wait a minute, this isn't an admin... HALP!
+
+Sign Out: <a href="#" onclick="logout();">Sign Out</a>
+
+Accounts Button: <?php if($this->isSU()){?><li><a href="index.php?p=admin&action=manageaccounts">Accounts</a></li><?php }?>
+
+Check Page Name And If It's Equal To x Then Do Something:
+<?php if(get_class($this) == "home"){echo "active";}?>
+Reads the ?p=PAGENAME
+
--- application/views/_template/header.php
+++ application/views/_template/header.php
@@ -23,26 +23,6 @@
 			</div>
 			
 			<!-- BEGIN Navbar -->
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse"
-					id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li class="<?php if(get_class($this) == "home"){echo "active";}?>"><a href="index.php">Home</a></li>
-						<li class="<?php if(get_class($this) == "ban"){echo "active";}?>"><a href="index.php?p=ban">Bans</a></li>
-						<li class="<?php if(get_class($this) == "mute"){echo "active";}?>"><a href="index.php?p=mute">Mutes</a></li>
-						<li class="<?php if(get_class($this) == "kick"){echo "active";}?>"><a href="index.php?p=kick">Kicks</a></li>
-						<li class="<?php if(get_class($this) == "comment"){echo "active";}?>"><a href="index.php?p=comment">Comments</a></li>
-						<?php if($this->isAdmin()) {include("admin/navbarAddon.php");} else { echo "<li class='divider-vertical'></li><li><a href='?p=admin'>Login</a></li>";}?>
-					</ul>
-					<form class="navbar-form navbar-right" method="get">
-						<div class="form-group">
-							<input type="text" class="form-control input-text" id="pname-input" name="player" placeholder="Player Name">
-						</div>
-						<input type="hidden" name="p" value="profile">
-						<button type="submit" class="btn btn-default btn-bat">View profile</button>
-					</form>
-				</div>
-				
 				<nav class="navbar navbar-default navbar-fixed-top">
 					<div class="container" id="bs-example-navbar-collapse-1">
 						<!-- Brand and toggle get grouped for better mobile display -->
@@ -53,53 +33,25 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 							</button>
-							<a class="navbar-brand" href="./">CP2 Interface</a>
+							<a class="navbar-brand" href="./">BAT Interface</a>
 						</div>
 						
 						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse" id="main_navbar_collapse">
 							<ul class="nav navbar-nav">
-							<?php if ($shownavs) foreach($this->c['navbar'] as $ll => $hf) echo '<li><a href="'.$hf.'">'.$ll.'</a></li>';?>
+								<li class="<?php if(get_class($this) == "home"){echo "active";}?>"><a href="index.php">Home</a></li>
+								<li class="<?php if(get_class($this) == "ban"){echo "active";}?>"><a href="index.php?p=ban">Bans</a></li>
+								<li class="<?php if(get_class($this) == "mute"){echo "active";}?>"><a href="index.php?p=mute">Mutes</a></li>
+								<li class="<?php if(get_class($this) == "kick"){echo "active";}?>"><a href="index.php?p=kick">Kicks</a></li>
+								<li class="<?php if(get_class($this) == "comment"){echo "active";}?>"><a href="index.php?p=comment">Comments</a></li>
 							</ul>
 							<!-- User control (right side of navbar) -->
 							<ul class="nav navbar-nav navbar-right">
-								<li class="dropdown alert-dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span style="margin: -10px 0px; font-size: 16px;"><i class="fa fa-envelope"></i> <div style="display: inline;" id="pms"></div></span></a>
-								<ul class="dropdown-menu">
-									<li><a href="#">Click View Messages</a></li>
-									<li role="separator" class="divider"></li>
-									<li><a href="../user/messaging">View Messages</a></li>
-								</ul>
-								</li>	
-								
-								<li class="dropdown alert-dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span style="margin: -10px 0px; font-size: 16px;"><i class="fa fa-flag"></i> <div style="display: inline;" id="alerts"></div></span></a>
-								<ul class="dropdown-menu">
-									<li><a href="#">Click View Alerts</a></li>
-									<li role="separator" class="divider"></li>
-									<li><a href="../user/alerts">View Alerts</a></li>
-								</ul>
-								</li>
-								
-								<li class="dropdown">
-								<a href="#" data-toggle="dropdown" role="button" aria-expanded="false">&nbsp;&nbsp;MuhsinunCool <span class="caret"></span></a>
-								<ul class="dropdown-menu" role="menu">
-									<li><a href="../profile/<?php echo $this->u;?>">Profile</a></li>
-									<li class="divider"></li>
-									<li><a href="../user">UserCP</a></li>
-									<li><a href="../mod">ModCP</a></li>
-									<li><a href="../admin">AdminCP</a></li>
-									<li class="divider"></li>
-									<li><a href="../infractions">Infractions</a></li>
-									<li class="divider"></li>
-									<li><a href="./login.php<?php if ($this->u) echo "?action=logout"?>"><?php echo $this->u ? "Sign Out" : "Sign In";?></a></li>
-								</ul>
-								</li>
+								<?php if($this->isAdmin()) {include("admin/navbarAddon.php");} else { echo "<li><a href='?p=admin'>Login</a></li>";}?>
 							</ul>
 						</div><!-- /.navbar-collapse -->
 					</div><!-- /.container -->
 				</nav>
-				
 			<!-- END Navbar -->
 		</div>
 	</nav>
