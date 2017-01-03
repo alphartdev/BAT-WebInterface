c67cd24c23a66d15c7da1e0a41b837feae3de637
Muhsinun Chowdhury <muhsinun08@gmail.com>
2017-01-03 18:46:33 -0500

    trying to add ability to go to ?p=themes

--- application/views/_template/header.php
+++ application/views/_template/header.php
@@ -14,12 +14,160 @@
 	<!-- Additional CSS and JS -->
 	<link rel="stylesheet" href="public/styles/base-stylesheet.css">
 	<script src="public/js/base-script.js"></script>
-	<!-- BEGIN CUSTOM THEMES -->
+	<!-- CUSTOM THEMES HERE!!! -->
 		<!-- Yeti - DEFAULT -->
 			<!-- For Development -->
 			<link rel="stylesheet" href="res/themes/Yeti/css/bootstrap.min.css?version=2" title="Yeti">
 			<link rel="stylesheet" href="res/themes/Yeti/css/custom.css?version=2" title="Yeti">
 			<link rel="stylesheet" href="res/themes/Yeti/css/font-awesome.min.css?version=2" title="Yeti">
+			<!-- Live CSS -->
+			<link rel="stylesheet" href="/styles/themes/Yeti/css/bootstrap.min.css?version=2" title="Yeti">
+			<link rel="stylesheet" href="/styles/themes/Yeti/css/custom.css?version=2" title="Yeti">
+			<link rel="stylesheet" href="/styles/themes/Yeti/css/font-awesome.min.css?version=2" title="Yeti">
+		<!-- Bootstrap -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Bootstrap/css/bootstrap.min.css?version=2" title="Bootstrap">
+			<link rel="alternate stylesheet" href="res/themes/Bootstrap/css/custom.css?version=2" title="Bootstrap">
+			<link rel="alternate stylesheet" href="res/themes/Bootstrap/css/font-awesome.min.css?version=2" title="Bootstrap">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Bootstrap/css/bootstrap.min.css?version=2" title="Bootstrap">
+			<link rel="alternate stylesheet" href="/styles/themes/Bootstrap/css/custom.css?version=2" title="Bootstrap">
+			<link rel="alternate stylesheet" href="/styles/themes/Bootstrap/css/font-awesome.min.css?version=2" title="Bootstrap">
+		<!-- Cerulean -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Cerulean/css/bootstrap.min.css?version=2" title="Cerulean">
+			<link rel="alternate stylesheet" href="res/themes/Cerulean/css/custom.css?version=2" title="Cerulean">
+			<link rel="alternate stylesheet" href="res/themes/Cerulean/css/font-awesome.min.css?version=2" title="Cerulean">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Cerulean/css/bootstrap.min.css?version=2" title="Cerulean">
+			<link rel="alternate stylesheet" href="/styles/themes/Cerulean/css/custom.css?version=2" title="Cerulean">
+			<link rel="alternate stylesheet" href="/styles/themes/Cerulean/css/font-awesome.min.css?version=2" title="Cerulean">
+		<!-- Cosmo -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Cosmo/css/bootstrap.min.css?version=2" title="Cosmo">
+			<link rel="alternate stylesheet" href="res/themes/Cosmo/css/custom.css?version=2" title="Cosmo">
+			<link rel="alternate stylesheet" href="res/themes/Cosmo/css/font-awesome.min.css?version=2" title="Cosmo">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Cosmo/css/bootstrap.min.css?version=2" title="Cosmo">
+			<link rel="alternate stylesheet" href="/styles/themes/Cosmo/css/custom.css?version=2" title="Cosmo">
+			<link rel="alternate stylesheet" href="/styles/themes/Cosmo/css/font-awesome.min.css?version=2" title="Cosmo">
+		<!-- Cyborg -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Cyborg/css/bootstrap.min.css?version=2" title="Cyborg">
+			<link rel="alternate stylesheet" href="res/themes/Cyborg/css/custom.css?version=2" title="Cyborg">
+			<link rel="alternate stylesheet" href="res/themes/Cyborg/css/font-awesome.min.css?version=2" title="Cyborg">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Cyborg/css/bootstrap.min.css?version=2" title="Cyborg">
+			<link rel="alternate stylesheet" href="/styles/themes/Cyborg/css/custom.css?version=2" title="Cyborg">
+			<link rel="alternate stylesheet" href="/styles/themes/Cyborg/css/font-awesome.min.css?version=2" title="Cyborg">
+		<!-- Darkly -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Darkly/css/bootstrap.min.css?version=2" title="Darkly">
+			<link rel="alternate stylesheet" href="res/themes/Darkly/css/custom.css?version=2" title="Darkly">
+			<link rel="alternate stylesheet" href="res/themes/Darkly/css/font-awesome.min.css?version=2" title="Darkly">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Darkly/css/bootstrap.min.css?version=2" title="Darkly">
+			<link rel="alternate stylesheet" href="/styles/themes/Darkly/css/custom.css?version=2" title="Darkly">
+			<link rel="alternate stylesheet" href="/styles/themes/Darkly/css/font-awesome.min.css?version=2" title="Darkly">
+		<!-- Flatly -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Darkly/css/bootstrap.min.css?version=2" title="Darkly">
+			<link rel="alternate stylesheet" href="res/themes/Darkly/css/custom.css?version=2" title="Darkly">
+			<link rel="alternate stylesheet" href="res/themes/Darkly/css/font-awesome.min.css?version=2" title="Darkly">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Flatly/css/bootstrap.min.css?version=2" title="Flatly">
+			<link rel="alternate stylesheet" href="/styles/themes/Flatly/css/custom.css?version=2" title="Flatly">
+			<link rel="alternate stylesheet" href="/styles/themes/Flatly/css/font-awesome.min.css?version=2" title="Flatly">
+		<!-- Journal -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Journal/css/bootstrap.min.css?version=2" title="Journal">
+			<link rel="alternate stylesheet" href="res/themes/Journal/css/custom.css?version=2" title="Journal">
+			<link rel="alternate stylesheet" href="res/themes/Journal/css/font-awesome.min.css?version=2" title="Journal">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Journal/css/bootstrap.min.css?version=2" title="Journal">
+			<link rel="alternate stylesheet" href="/styles/themes/Journal/css/custom.css?version=2" title="Journal">
+			<link rel="alternate stylesheet" href="/styles/themes/Journal/css/font-awesome.min.css?version=2" title="Journal">
+		<!-- Lumen -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Lumen/css/bootstrap.min.css?version=2" title="Lumen">
+			<link rel="alternate stylesheet" href="res/themes/Lumen/css/custom.css?version=2" title="Lumen">
+			<link rel="alternate stylesheet" href="res/themes/Lumen/css/font-awesome.min.css?version=2" title="Lumen">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Lumen/css/bootstrap.min.css?version=2" title="Lumen">
+			<link rel="alternate stylesheet" href="/styles/themes/Lumen/css/custom.css?version=2" title="Lumen">
+			<link rel="alternate stylesheet" href="/styles/themes/Lumen/css/font-awesome.min.css?version=2" title="Lumen">
+		<!-- Paper -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Paper/css/bootstrap.min.css?version=2" title="Paper">
+			<link rel="alternate stylesheet" href="res/themes/Paper/css/custom.css?version=2" title="Paper">
+			<link rel="alternate stylesheet" href="res/themes/Paper/css/font-awesome.min.css?version=2" title="Paper">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Paper/css/bootstrap.min.css?version=2" title="Paper">
+			<link rel="alternate stylesheet" href="/styles/themes/Paper/css/custom.css?version=2" title="Paper">
+			<link rel="alternate stylesheet" href="/styles/themes/Paper/css/font-awesome.min.css?version=2" title="Paper">
+		<!-- Readable -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Readable/css/bootstrap.min.css?version=2" title="Readable">
+			<link rel="alternate stylesheet" href="res/themes/Readable/css/custom.css?version=2" title="Readable">
+			<link rel="alternate stylesheet" href="res/themes/Readable/css/font-awesome.min.css?version=2" title="Readable">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Readable/css/bootstrap.min.css?version=2" title="Readable">
+			<link rel="alternate stylesheet" href="/styles/themes/Readable/css/custom.css?version=2" title="Readable">
+			<link rel="alternate stylesheet" href="/styles/themes/Readable/css/font-awesome.min.css?version=2" title="Readable">
+		<!-- Sandstone -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Sandstone/css/bootstrap.min.css?version=2" title="Sandstone">
+			<link rel="alternate stylesheet" href="res/themes/Sandstone/css/custom.css?version=2" title="Sandstone">
+			<link rel="alternate stylesheet" href="res/themes/Sandstone/css/font-awesome.min.css?version=2" title="Sandstone">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Sandstone/css/bootstrap.min.css?version=2" title="Sandstone">
+			<link rel="alternate stylesheet" href="/styles/themes/Sandstone/css/custom.css?version=2" title="Sandstone">
+			<link rel="alternate stylesheet" href="/styles/themes/Sandstone/css/font-awesome.min.css?version=2" title="Sandstone">
+		<!-- Simplex -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Simplex/css/bootstrap.min.css?version=2" title="Simplex">
+			<link rel="alternate stylesheet" href="res/themes/Simplex/css/custom.css?version=2" title="Simplex">
+			<link rel="alternate stylesheet" href="res/themes/Simplex/css/font-awesome.min.css?version=2" title="Simplex">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Simplex/css/bootstrap.min.css?version=2" title="Simplex">
+			<link rel="alternate stylesheet" href="/styles/themes/Simplex/css/custom.css?version=2" title="Simplex">
+			<link rel="alternate stylesheet" href="/styles/themes/Simplex/css/font-awesome.min.css?version=2" title="Simplex">
+		<!-- Slate -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Slate/css/bootstrap.min.css?version=2" title="Slate">
+			<link rel="alternate stylesheet" href="res/themes/Slate/css/custom.css?version=2" title="Slate">
+			<link rel="alternate stylesheet" href="res/themes/Slate/css/font-awesome.min.css?version=2" title="Slate">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Slate/css/bootstrap.min.css?version=2" title="Slate">
+			<link rel="alternate stylesheet" href="/styles/themes/Slate/css/custom.css?version=2" title="Slate">
+			<link rel="alternate stylesheet" href="/styles/themes/Slate/css/font-awesome.min.css?version=2" title="Slate">
+		<!-- Spacelab -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Spacelab/css/bootstrap.min.css?version=2" title="Spacelab">
+			<link rel="alternate stylesheet" href="res/themes/Spacelab/css/custom.css?version=2" title="Spacelab">
+			<link rel="alternate stylesheet" href="res/themes/Spacelab/css/font-awesome.min.css?version=2" title="Spacelab">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Spacelab/css/bootstrap.min.css?version=2" title="Spacelab">
+			<link rel="alternate stylesheet" href="/styles/themes/Spacelab/css/custom.css?version=2" title="Spacelab">
+			<link rel="alternate stylesheet" href="/styles/themes/Spacelab/css/font-awesome.min.css?version=2" title="Spacelab">
+		<!-- Superhero -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/Superhero/css/bootstrap.min.css?version=2" title="Superhero">
+			<link rel="alternate stylesheet" href="res/themes/Superhero/css/custom.css?version=2" title="Superhero">
+			<link rel="alternate stylesheet" href="res/themes/Superhero/css/font-awesome.min.css?version=2" title="Superhero">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Superhero/css/bootstrap.min.css?version=2" title="Superhero">
+			<link rel="alternate stylesheet" href="/styles/themes/Superhero/css/custom.css?version=2" title="Superhero">
+			<link rel="alternate stylesheet" href="/styles/themes/Superhero/css/font-awesome.min.css?version=2" title="Superhero">
+		<!-- United -->
+			<!-- For Development -->
+			<link rel="alternate stylesheet" href="res/themes/United/css/bootstrap.min.css?version=2" title="United">
+			<link rel="alternate stylesheet" href="res/themes/United/css/custom.css?version=2" title="United">
+			<link rel="alternate stylesheet" href="res/themes/United/css/font-awesome.min.css?version=2" title="United">
+			<!-- Live CSS -->
+			<link rel="alternate stylesheet" href="/styles/themes/Journal/css/bootstrap.min.css?version=2" title="United">
+			<link rel="alternate stylesheet" href="/styles/themes/Journal/css/custom.css?version=2" title="United">
+			<link rel="alternate stylesheet" href="/styles/themes/Journal/css/font-awesome.min.css?version=2" title="United">
 	<!-- END CUSTOM THEMES -->
 	<link rel="stylesheet" href="public/styles/custom.css">
 </head>
